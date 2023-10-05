ef73cd64d2a8ac9ea2ad746f255c5f8182fbd4fd
ArunKumar2019 <48507486+ArunKumar2019@users.noreply.github.com>
2023-10-05 13:47:32 +0530

    Add files via upload

---
+++ project-blog-rest-api/src/main/java/com/backend/blog/projectblogrestapi/ProjectBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.blog.projectblogrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBlogRestApiApplication.class, args);
+	}
+
+}
---
+++ project-blog-rest-api/src/test/java/com/backend/blog/projectblogrestapi/ProjectBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.blog.projectblogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
